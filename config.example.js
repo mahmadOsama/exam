fdc8302493f39ebb84ce9861a0cde45fb6c5872a
mahmadOsama <mahmadxxx888@gmail.com>
2026-06-15 17:21:10 +0300

    Fix Supabase config example

--- config.example.js
+++ config.example.js
@@ -2,6 +2,6 @@
 // Do NOT commit your real config.js.
 
 export const SUPABASE_URL = 'https://sxcvqimdmonthsvxqfqu.supabase.co';
-export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN4Y3ZxaW1kbW9udGhzdnhxZnF1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1MDI2ODIsImV4cCI6MjA5NzA3ODY4Mn0.YCSnUTtXlVzCl3-hUAYIix87Ak06ueD3EmAA4-nVCaI';
+export const SUPABASE_ANON_KEY ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN4Y3ZxaW1kbW9udGhzdnhxZnF1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1MDI2ODIsImV4cCI6MjA5NzA3ODY4Mn0.YCSnUTtXlVzCl3-hUAYIix87Ak06ueD3EmAA4-nVCaI';
 
 
